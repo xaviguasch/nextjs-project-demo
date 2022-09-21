4149e3306c5597eccfa2063d8cd86f6b7e761745
xaviguasch <xaviguasch@gmail.com>
2022-09-21 23:18:21 +0200

    added dynamic paths and routes

---
+++ pages/portfolio/[projectId].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PortFolioProjectPage = () => {
+  return (
+    <div>
+      <h1>The Portfolio Project Page</h1>
+    </div>
+  )
+}
+
+export default PortFolioProjectPage
