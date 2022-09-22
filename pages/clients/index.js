5f2841d3348aba9dff60c384047e3db9b29e601d
xaviguasch <xaviguasch@gmail.com>
2022-09-22 16:21:58 +0200

    added a different way of setting Link hrefs

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import Link from 'next/link'
 
 const ClientsPage = () => {
+  const clients = [
+    { id: 'max', name: 'Maximilian' },
+    { id: 'manu', name: 'Manuel' },
+  ]
+
   return (
     <div>
       <h1>The Clients Page</h1>
+
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link
+                href={{
+                  pathname: '/clients/[id]',
+                  query: { id: client.id },
+                }}
+              >
+                {client.name}
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
