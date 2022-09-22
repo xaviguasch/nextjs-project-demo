303160bc84a772c20d83d095a4d08a77cbd80646
xaviguasch <xaviguasch@gmail.com>
2022-09-22 14:47:35 +0200

    added catch-all routes

---
+++ pages/clients/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClientsPage = () => {
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+    </div>
+  )
+}
+
+export default ClientsPage
--- pages/portfolio/[projectId].js
+++ pages/portfolio/[projectId].js
@@ -2,6 +2,10 @@ import { useRouter } from 'next/router'
 
 const PortFolioProjectPage = () => {
   const router = useRouter()
+
+  console.log(router.pathname)
+  console.log(router.query)
+
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
